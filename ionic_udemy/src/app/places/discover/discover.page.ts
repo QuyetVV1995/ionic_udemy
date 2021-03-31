858970893f9884f073d6cb7cf7e54b180f19fb68
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-31 15:45:17 +0700

    144

--- ionic_udemy/src/app/places/discover/discover.page.ts
+++ ionic_udemy/src/app/places/discover/discover.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { from } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-discover',
@@ -11,15 +13,22 @@ import { PlacesService } from '../places.service';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlace: Place[];
+
   constructor(private placesService: PlacesService,
     private menuCtrl: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlace = this.loadedPlaces.slice(1);
   }
 
   onOpenMenu(){
     this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>){
+    console.log(event.detail);
   }
 
 }
