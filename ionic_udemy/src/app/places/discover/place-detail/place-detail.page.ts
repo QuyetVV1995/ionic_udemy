7024ba613f31d77ff265ee388e3ad7c88f424d83
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-31 13:43:35 +0700

    128

--- ionic_udemy/src/app/places/discover/place-detail/place-detail.page.ts
+++ ionic_udemy/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,17 +12,46 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router,
-    private navCtrl: NavController) { }
+  place: Place;
+
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/discover');
+
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      console.log(this.place);
+    });
   }
 
   onBookPlaces(){
 
     // this.router.navigateByUrl('/places/discover');
-    this.navCtrl.navigateBack('/places/discover');
+    // this.navCtrl.navigateBack('/places/discover');
     // this.navCtrl.pop();
+    this.modalCtrl.create({ component: CreateBookingComponent,
+      componentProps: {selectedPlace: this.place},
+
+    })
+    .then(modelEl => {
+      modelEl.present();
+      return modelEl.onDidDismiss();
+    })
+    .then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if(resultData.role === 'confirm'){
+        console.log('BOOKED!');
+      }
+    });
   }
 
 }
