2b885b122d305416dcc43ea6b7cc6d5be1f43cbf
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-03-31 21:05:59 +0700

    172

--- ionic_udemy/src/app/auth/auth.page.ts
+++ ionic_udemy/src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -11,6 +12,7 @@ import { AuthService } from './auth.service';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService,
       private router: Router,
@@ -31,6 +33,26 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places');
       }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm){
+    if(!form.valid){
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if(this.isLogin){
+      // Send a request to login servers
+    }else{
+      //  Send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode(){
+    this.isLogin = !this.isLogin;
   }
 
 }
