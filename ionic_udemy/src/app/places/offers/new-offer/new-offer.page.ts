a80c0e7004556789e0991db610184cdc3dab2b9f
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-04-04 20:22:42 +0700

    249

--- ionic_udemy/src/app/places/offers/new-offer/new-offer.page.ts
+++ ionic_udemy/src/app/places/offers/new-offer/new-offer.page.ts
@@ -4,6 +4,27 @@ import { Router } from '@angular/router';
 import { PlaceLocation } from '../../location.model';
 import { PlacesService } from '../../places.service';
 
+function base64toBlob(base64Data, contentType) {
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = window.atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
+
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
@@ -41,14 +62,16 @@ export class NewOfferPage implements OnInit {
       }),
       location: new FormControl(null, {
         validators: [Validators.required]
-      })
+      }),
+      image: new FormControl(null)
     });
   }
 
   onCreateOffer() {
-    if (!this.form.valid) {
+    if (!this.form.valid || !this.form.get('image').value) {
       return;
     }
+    console.log(this.form.value);
     this.placeService.addPlace(
       this.form.value.title,
       this.form.value.description,
@@ -64,5 +87,21 @@ export class NewOfferPage implements OnInit {
 
   onLocationPicked(location: PlaceLocation){
     this.form.patchValue({location: location});
+  }
+
+  onImagePick(imageData: string | File){
+    let imageFile;
+    if(typeof imageData === 'string'){
+      try {
+        imageFile = base64toBlob(imageData.replace('data:image/jped;base64',''), 'image/jpeg');
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    }else{
+      imageFile = imageData;
+    }
+    this.form.patchValue({image: imageFile});
+
   }
 }
