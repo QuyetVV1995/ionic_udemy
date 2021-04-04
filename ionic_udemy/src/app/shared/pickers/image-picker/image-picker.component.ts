66d62d708e236f16ec77c1a9f747d16991c0b434
QuyetVV1995 <quyeta2ubqn2gmail.com>
2021-04-04 20:44:40 +0700

    fixbug 250

--- ionic_udemy/src/app/shared/pickers/image-picker/image-picker.component.ts
+++ ionic_udemy/src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -57,7 +57,7 @@ export class ImagePickerComponent implements OnInit {
       resultType: CameraResultType.Base64
     })
       .then(image => {
-        this.selectedImage = image.base64String;
+        this.selectedImage = 'data:image/jpeg;base64,' + image.base64String;
         this.imagePick.emit(image.base64String);
       })
       .catch(error => {
